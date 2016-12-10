69217d51e7df7a86e2e7d547da3cdc487d1aa1c0
Ngoc Do <nguyenanhngoc.do@gmail.com>
2016-12-10 12:55:13 -0800

    First commit for CODEIGNITER with HMVC

---
+++ application/modules/blog/controllers/Page.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Page extends CI_Controller {
+
+    public function index()
+    {
+        echo 'page index';
+    }
+}
