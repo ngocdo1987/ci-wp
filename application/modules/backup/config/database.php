1400b8c8168be3d1279313327eaed65f56481f72
Ngoc Do <nguyenanhngoc.do@gmail.com>
2016-12-11 01:41:13 -0800

    Backup manager

---
+++ application/modules/backup/config/database.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'development' => [
+        'type' => 'mysql',
+        'host' => 'localhost',
+        'port' => '3306',
+        'user' => 'root',
+        'pass' => '',
+        'database' => 'fw_codeigniter',
+    ],
+    'production' => [
+        'type' => 'mysql',
+        'host' => 'localhost',
+        'port' => '3306',
+        'user' => 'root',
+        'pass' => '',
+        'database' => 'fw_codeigniter',
+    ],
+];
---
+++ application/modules/backup/controllers/Backup.php
@@ -0,0 +1,57 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+use BackupManager\Config\Config;
+use BackupManager\Filesystems;
+use BackupManager\Databases;
+use BackupManager\Compressors;
+use BackupManager\Manager;
+use BackupManager\Filesystems\Destination;
+
+class Backup extends CI_Controller {
+    private $manager;
+
+	function __construct()
+	{
+		parent::__construct();
+
+        $filesystems = new Filesystems\FilesystemProvider(Config::fromPhpFile(__DIR__.'/../config/storage.php'));
+        $filesystems->add(new Filesystems\Awss3Filesystem);
+        $filesystems->add(new Filesystems\GcsFilesystem);
+        $filesystems->add(new Filesystems\DropboxFilesystem);
+        $filesystems->add(new Filesystems\FtpFilesystem);
+        $filesystems->add(new Filesystems\LocalFilesystem);
+        $filesystems->add(new Filesystems\RackspaceFilesystem);
+        $filesystems->add(new Filesystems\SftpFilesystem);
+
+        $databases = new Databases\DatabaseProvider(Config::fromPhpFile(__DIR__.'/../config/database.php'));
+        $databases->add(new Databases\MysqlDatabase);
+        $databases->add(new Databases\PostgresqlDatabase);
+
+        $compressors = new Compressors\CompressorProvider;
+        $compressors->add(new Compressors\GzipCompressor);
+        $compressors->add(new Compressors\NullCompressor);
+
+        $this->manager = new Manager($filesystems, $databases, $compressors);
+	}
+
+    function index()
+    {
+        $this->manager->makeBackup()
+        ->run('development', [
+            new Destination('local', 'backup-'.date('Y-m-d-H-i-s').'.sql'),
+            //new Destination('s3', 'test/dump.sql')
+        ], 'gzip');
+    }
+
+    function backup()
+    {
+
+    }
+
+    function restore()
+    {
+        
+    }
+
+}
